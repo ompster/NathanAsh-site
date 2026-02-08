b2135bbf36ed6840ce492d47d12a70357b4a2e8e
Nathan Ash <nathan.ash@gmail.com>
2026-02-08 17:53:00 +1100

    Initial commit: Astro + Spectre theme

---
+++ astro.config.ts
@@ -0,0 +1,37 @@
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+import expressiveCode from 'astro-expressive-code';
+import spectre from './package/src';
+import { spectreDark } from './src/ec-theme';
+
+// https://astro.build/config
+const config = defineConfig({
+	site: 'https://nathanash.id.au',
+	output: 'static',
+	integrations: [
+		expressiveCode({
+			themes: [spectreDark],
+		}),
+		mdx(),
+		sitemap(),
+		spectre({
+			name: 'Nathan Ash',
+			openGraph: {
+				home: {
+					title: 'Nathan Ash',
+					description: 'Technical Services Manager & Systems Engineer based in Melbourne.',
+				},
+				blog: {
+					title: 'Blog',
+					description: 'Technical posts, homelab adventures, and learnings.',
+				},
+				projects: {
+					title: 'Projects',
+				},
+			},
+		}),
+	],
+});
+
+export default config;
